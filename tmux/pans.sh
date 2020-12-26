46581d36b3e3efc23e513051780a69b7dfc30b65
jesuserro <jeigatwork@gmail.com>
2020-12-26 17:21:51 +0100

    feat(TMUX): Reordeing file and go to html folder

--- tmux/pans.sh
+++ tmux/pans.sh
@@ -11,10 +11,12 @@ tmux send-keys -t 2 'tail -f /var/log/nginx/error.log' C-m && sleep $tiempo
 # ---------------------------------------------------------------------------
 
 tmux new-window -t 2 -n 'BBDD'
+tmux send -t $session:BBDD "sudo mysql -u root -p" ENTER
 
 tmux select-window -t 1
 tmux select-pane -t 1
-tmux send -t $session:BBDD "sudo mysql -u root -p" ENTER
+
+tmux send -t $session:Debug "cd /var/www/html" ENTER
 
 # Styles:
 # border colours: tmux set -g pane-border-style fg=red
