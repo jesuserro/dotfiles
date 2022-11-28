e806746f437b96a325f4018a0d7a69309d8192e8
olagato <olagato@gmail.com>
2022-11-28 12:33:52 +0100

    feat(tmux): Added new tabs

--- tmux/pans.sh
+++ tmux/pans.sh
@@ -7,21 +7,39 @@ tiempo=0.5
 
 ip=$(hostname -I)
 sesion=${ip//./_}
-
 tmux new-session -d -s $sesion && sleep $tiempo # Crea panel t1
-tmux rename-window "Debug"
-tmux split-window -v -p 35 && sleep $tiempo # Crea t2
-tmux send-keys -t 2 'tail -f /var/log/apache2/error.log' C-m && sleep $tiempo
-# ---------------------------------------------------------------------------
 
-tmux new-window -t 2 -n 'BBDD'
+# Total screen
+tmux rename-window "One"
+
+# Split screen
+tmux new-window -t 2 -n 'Two'
+tmux split-window -v -p 35 && sleep $tiempo # Crea t2
+
+# BBDD
+tmux new-window -t 3 -n 'BBDD'
 tmux send -t $session:BBDD "sudo -S ssh -o 'IdentitiesOnly yes' -i ~/.ssh/aws-jesuserro-key.pem -L 3333:localhost:3306 ubuntu@libricos.com" C-m && sleep $tiempo
 # tmux send -t $session:BBDD "sudo mysql -u root -p" ENTER
 
+# Local NGES
+tmux new-window -t 4 -n 'NGES'
+tmux send -t $session:NGES "cd /var/www/nges.local && ss" ENTER
+tmux split-window -v -p 35 && sleep $tiempo # Crea t3
+tmux send-keys -t 4 'tail -f /var/log/apache2/error.log' C-m && sleep $tiempo
+
+# Ofertas
+tmux new-window -t 5 -n 'Ofertas'
+tmux send -t $session:Ofertas "cd /var/www/ofertas.cdrst.local" ENTER
+tmux split-window -v -p 35 && sleep $tiempo 
+tmux send-keys -t 5 'ssh -oHostKeyAlgorithms=+ssh-dss subv@89.17.208.189' C-m && sleep $tiempo
+
+
+
+
+
+# Selección final
 tmux select-window -t 1
 tmux select-pane -t 1
-
-tmux send -t $session:Debug "cd /var/www/nges.local && ss" ENTER
 
 # Styles
 # Border colours 
