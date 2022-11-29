5ec815bfcf36885a3826fd91e739149c0fc680e9
olagato <olagato@gmail.com>
2022-11-29 08:24:58 +0100

    feat(tmux): added nges

---
+++ tmux/nges.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ~/dotfiles/tmux/common/header.sh
+
+
+# Local
+tmux rename-window "Local"
+tmux send -t $session:Local "cd /var/www/nges.local" ENTER
+tmux split-window -v -p 35 && sleep $tiempo
+tmux send-keys 'tail -f /var/log/apache2/error.log' C-m && sleep $tiempo
+
+# DEV 
+tmux new-window -t 2 -n '89.17.208.189'
+tmux send-keys -t 2 'ssh -oHostKeyAlgorithms=+ssh-dss subv@89.17.208.189' C-m && sleep $tiempo
+tmux split-window -v -p 35 && sleep $tiempo 
+tmux send-keys -t 2 'ssh -oHostKeyAlgorithms=+ssh-dss subv@89.17.208.189 && cd /home && tail -f /var/log/httpd/dev-ofertas_error_log' C-m && sleep $tiempo
+
+
+source ~/dotfiles/tmux/common/footer.sh
+
+
+ 
