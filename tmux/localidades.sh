7825fcaa015c63e6c58c50ebb73fb87a28cc51a7
olagato <olagato@gmail.com>
2022-12-01 08:38:12 +0100

    feat(tmux): added localidades

---
+++ tmux/localidades.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ~/dotfiles/tmux/common/header.sh
+
+
+# Local
+tmux rename-window "Local"
+tmux send -t $session:Local "cd /var/www/localidades.cdrst.local" ENTER
+tmux split-window -v -p 35 && sleep $tiempo
+tmux send-keys 'tail -f /var/log/apache2/error.log' C-m && sleep $tiempo
+
+# PROD 
+tmux new-window -t 2 -n 'ms.cdrst.com'
+tmux send-keys -t 2 'ssh -oHostKeyAlgorithms=+ssh-dss subv@ms.cdrst.com' C-m && sleep $tiempo
+tmux split-window -v -p 35 && sleep $tiempo 
+tmux send-keys -t 2 'ssh -oHostKeyAlgorithms=+ssh-dss subv@ms.cdrst.com && tail -f /var/log/httpd/dev-ofertas_error_log' C-m && sleep $tiempo
+
+
+source ~/dotfiles/tmux/common/footer.sh
+
+
+ 
