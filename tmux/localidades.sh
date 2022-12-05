978e0d562d449afec2283b54f02dcde02e772854
olagato <olagato@gmail.com>
2022-12-05 14:50:55 +0100

    feat(tmux localidades): added log panel

--- tmux/localidades.sh
+++ tmux/localidades.sh
@@ -13,7 +13,8 @@ tmux send-keys 'tail -f /var/log/apache2/error.log' C-m && sleep $tiempo
 tmux new-window -t 2 -n 'ms.cdrst.com'
 tmux send-keys -t 2 'ssh -oHostKeyAlgorithms=+ssh-dss jesus@ms.cdrst.com' C-m && sleep $tiempo
 tmux split-window -v -p 35 && sleep $tiempo 
-tmux send-keys -t 2 'ssh -oHostKeyAlgorithms=+ssh-dss jesus@ms.cdrst.com && tail -f /var/log/httpd/dev-ofertas_error_log' C-m && sleep $tiempo
+tmux send-keys -t 2 'ssh -oHostKeyAlgorithms=+ssh-dss jesus@ms.cdrst.com' C-m && sleep $tiempo
+tmux send-keys -t 2 'tail -f /var/log/httpd/localidades_error_log' C-m && sleep $tiempo
 
 
 source ~/dotfiles/tmux/common/footer.sh
