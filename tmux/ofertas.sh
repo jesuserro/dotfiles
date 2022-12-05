48701084c754f9aac09f85b6970f3f5e84934720
olagato <olagato@gmail.com>
2022-12-05 11:27:26 +0100

    feat(tmux): change user, updated general, added ofertas

--- tmux/ofertas.sh
+++ tmux/ofertas.sh
@@ -11,9 +11,9 @@ tmux send-keys 'tail -f /var/log/apache2/error.log' C-m && sleep $tiempo
 
 # DEV 
 tmux new-window -t 2 -n '89.17.208.189'
-tmux send-keys -t 2 'ssh -oHostKeyAlgorithms=+ssh-dss subv@89.17.208.189' C-m && sleep $tiempo
+tmux send-keys -t 2 'ssh -oHostKeyAlgorithms=+ssh-dss jesus@89.17.208.189' C-m && sleep $tiempo
 tmux split-window -v -p 35 && sleep $tiempo 
-tmux send-keys -t 2 'ssh -oHostKeyAlgorithms=+ssh-dss subv@89.17.208.189 && tail -f /var/log/httpd/dev-ofertas_error_log' C-m && sleep $tiempo
+tmux send-keys -t 2 'ssh -oHostKeyAlgorithms=+ssh-dss jesus@89.17.208.189 && tail -f /var/log/httpd/dev-ofertas_error_log' C-m && sleep $tiempo
 
 
 source ~/dotfiles/tmux/common/footer.sh
--- tmux/work.sh
+++ tmux/work.sh
@@ -11,21 +11,21 @@ tmux new-window -t 2 -n 'Two'
 tmux split-window -v -p 35 && sleep $tiempo # Crea t2
 
 # BBDD
-tmux new-window -t 3 -n 'BBDD'
-tmux send -t $session:BBDD "sudo -S ssh -o 'IdentitiesOnly yes' -i ~/.ssh/aws-jesuserro-key.pem -L 3333:localhost:3306 ubuntu@libricos.com" C-m && sleep $tiempo
+# tmux new-window -t 3 -n 'BBDD'
+# tmux send -t $session:BBDD "sudo -S ssh -o 'IdentitiesOnly yes' -i ~/.ssh/aws-jesuserro-key.pem -L 3333:localhost:3306 ubuntu@libricos.com" C-m && sleep $tiempo
 # tmux send -t $session:BBDD "sudo mysql -u root -p" ENTER
 
 # Local NGES
-tmux new-window -t 4 -n 'NGES'
-tmux send -t $session:NGES "cd /var/www/nges.local && ss" ENTER
-tmux split-window -v -p 35 && sleep $tiempo # Crea t3
-tmux send-keys -t 4 'tail -f /var/log/apache2/error.log' C-m && sleep $tiempo
+# tmux new-window -t 4 -n 'NGES'
+# tmux send -t $session:NGES "cd /var/www/nges.local && ss" ENTER
+# tmux split-window -v -p 35 && sleep $tiempo # Crea t3
+# tmux send-keys -t 4 'tail -f /var/log/apache2/error.log' C-m && sleep $tiempo
 
 # Ofertas
-tmux new-window -t 5 -n 'Ofertas'
-tmux send -t $session:Ofertas "cd /var/www/ofertas.cdrst.local" ENTER
-tmux split-window -v -p 35 && sleep $tiempo 
-tmux send-keys -t 5 'ssh -oHostKeyAlgorithms=+ssh-dss subv@89.17.208.189' C-m && sleep $tiempo
+# tmux new-window -t 5 -n 'Ofertas'
+# tmux send -t $session:Ofertas "cd /var/www/ofertas.cdrst.local" ENTER
+# tmux split-window -v -p 35 && sleep $tiempo 
+# tmux send-keys -t 5 'ssh -oHostKeyAlgorithms=+ssh-dss jesus@89.17.208.189' C-m && sleep $tiempo
 
 
 source ~/dotfiles/tmux/common/footer.sh
