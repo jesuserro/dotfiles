2adb2ca229438789c4ce64c21b1441ff9880c1b4
Jesús Erro <jesus@centraldereservas.com>
2024-05-03 13:00:44 +0200

    chore: commit save point

--- powerlevel10k/p10k.zsh
+++ powerlevel10k/p10k.zsh
@@ -1,6 +1,10 @@
-# Configuración del color de fondo del directorio
+# Configuración del color: https://codehs.com/tutorial/ryan/add-color-with-ansi-in-javascript
+
 # typeset -g POWERLEVEL9K_DIR_BACKGROUND=166 # naranja oscuro
-typeset -g POWERLEVEL9K_DIR_BACKGROUND=172 # naranja oscuro
+# typeset -g POWERLEVEL9K_DIR_BACKGROUND=172 # naranja oscuro 2
+# typeset -g POWERLEVEL9K_DIR_BACKGROUND=216 # naranja pastel
+typeset -g POWERLEVEL9K_DIR_BACKGROUND=173 # naranja pastel
+
 
 # Configuración del color de fondo del os_icon
 typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=5 # Magenta
