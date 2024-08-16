c82eb549730cf9c7d4726df826eec283e08aad26
Jesús Erro <jesus@centraldereservas.com>
2024-08-16 16:56:50 +0200

    chore: commit save point

--- scripts/show_branches_with_dates.sh
+++ scripts/show_branches_with_dates.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Mostrar todas las ramas locales con la fecha del último commit
-echo "Ramas locales y su última actividad:"
-git for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short) - Última actividad: %(committerdate:format:%A, %d-%m-%Y %H:%M)'
+echo -e "\n\e[1;34mRamas locales y su última actividad:\e[0m"
+git for-each-ref --sort=-committerdate refs/heads/ --format='%(color:yellow)%(refname:short)%(color:reset) - Última actividad: %(color:green)%(committerdate:format:%A, %d-%m-%Y %H:%M)%(color:reset) - %(color:cyan)Autor: %(authorname)%(color:reset)'
+echo -e "\n\e[1;34mTotal de ramas: \e[1;32m$(git branch | wc -l)\e[0m"
