1b4d2aa6f1fe4eb967e8a9850d19bcd7f72fd167
Jesús Erro <jesus@centraldereservas.com>
2024-08-19 08:47:20 +0200

    chore: commit save point

--- scripts/system_info.sh
+++ scripts/system_info.sh
@@ -11,25 +11,25 @@ print_row() {
 }
 
 # Encabezado de la tabla
-echo -e "\n\e[1;34mInformación del Sistema:\e[0m"
+echo -e "\n\e[1;34mInformación Sistema:\e[0m"
 print_line
 print_row "Componente" "Detalle"
 print_line
 
 # Filas con información del sistema
-print_row "Nombre del PC" "$(hostname)"
+print_row "Nombre PC" "$(hostname)"
 print_row "Procesador" "$(lscpu | grep 'Model name:' | awk -F':' '{print $2}' | xargs)"
-print_row "Uso de Memoria RAM" "$(free -h | awk '/^Mem:/ {print $3 "/" $2}')"
-print_row "Uso del Disco Duro" "$(df -h --total | grep 'total' | awk '{print $3 "/" $2 " (" $5 " usado)"}')"
+print_row "Uso Memoria RAM" "$(free -h | awk '/^Mem:/ {print $3 "/" $2}')"
+print_row "Uso Disco Duro" "$(df -h --total | grep 'total' | awk '{print $3 "/" $2 " (" $5 " usado)"}')"
 print_row "Arquitectura" "$(uname -m)"
 print_row "Sistema Operativo" "$(lsb_release -d | cut -f2)"
-print_row "Versión del Kernel" "$(uname -r)"
-print_row "Uso de la CPU" "$(top -bn1 | grep 'Cpu(s)' | awk -F'id,' '{print 100 - $1"% usado"}')"
-print_row "Tiempo de actividad del sistema" "$(uptime -p)"
+print_row "Versión Kernel" "$(uname -r)"
+print_row "Uso CPU" "$(top -bn1 | grep 'Cpu(s)' | awk -F'id,' '{print 100 - $1"% usado"}')"
+print_row "Tiempo actividad" "$(uptime -p)"
 print_line
 
 # Estado de servicios
-echo -e "\n\e[1;34mEstado de Servicios:\e[0m"
+echo -e "\n\e[1;34mEstado Servicios:\e[0m"
 print_line
 print_row "Servicio" "Estado"
 print_line
@@ -40,11 +40,11 @@ print_row "SSH" "$(systemctl is-active ssh)"
 print_line
 
 # Conectividad a Internet e Interfaz de Red
-echo -e "\n\e[1;34mConectividad a Internet:\e[0m"
+echo -e "\n\e[1;34mConectividad Internet:\e[0m"
 print_line
 print_row "IP Local" "$(hostname -I | awk '{print $1}')"
 print_row "IP Pública" "$(curl -s https://api.ipify.org)"
-print_row "Interfaz de Red (eth0)" "$(ip addr | grep -Ee 'inet.*eth0' | awk '{print $2}')"
+print_row "Interfaz Red (eth0)" "$(ip addr | grep -Ee 'inet.*eth0' | awk '{print $2}')"
 if ping -c 3 google.com &> /dev/null; then
     print_row "Conectividad" "$(echo -e "\e[32mConectado\e[0m")"
 else
