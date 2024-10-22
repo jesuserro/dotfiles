c2fd668854aff6b2270c051b12e831e075c0c096
Jesús Erro <olagato@gmail.com>
2024-10-22 19:41:10 +0200

    chore: commit save point

--- termux/install.sh
+++ termux/install.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-# Cambiar mirrors a Europa (Alemania)
-echo "Cambiando mirrors a Europa (Alemania)..."
+# Cambiar mirrors a Europa
+echo "Cambiando mirrors a Europa..."
 termux-change-repo --stable-main --stable-universe --select-repo -r stable
 
-# Forzar el uso de la versión del mantenedor del paquete para evitar el conflicto de sources.list
-echo "Resolviendo conflictos con sources.list..."
-dpkg --configure -a || apt-get -o Dpkg::Options::="--force-confnew" --fix-broken install
+# Resolver posibles conflictos
+dpkg --configure -a
 
-# Actualizar e instalar las actualizaciones disponibles
-echo "Actualizando paquetes..."
+# Actualizar paquetes
+echo "Actualizando e instalando paquetes..."
 pkg update && pkg upgrade -y
 
 # Instalar paquetes básicos
-echo "Instalando paquetes básicos: git, lsd, unzip, openssh, nano, wget, curl..."
-pkg install -y git lsd unzip openssh nano wget curl
+pkg install -y git lsd unzip openssh nano wget curl zsh
 
-# Instalar Zsh
-echo "Instalando Zsh..."
-pkg install -y zsh
-
-# Verificar si Zsh está correctamente instalado antes de configurarlo como shell por defecto
+# Verificar Zsh
 if [ -x "$(command -v zsh)" ]; then
-    echo "Estableciendo Zsh como shell por defecto..."
     chsh -s zsh
+    echo "Zsh instalado y configurado como shell por defecto."
 else
-    echo "Error: Zsh no se ha instalado correctamente o no es ejecutable."
+    echo "Zsh no se instaló correctamente."
 fi
-
-# Confirmación de instalación
-echo "Instalación completada de los paquetes básicos y Zsh configurado como shell por defecto (si la instalación fue exitosa)."
