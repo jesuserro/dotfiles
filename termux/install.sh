bc2a3a2fb92db79c97f981bec878c624ca3c78dd
Jesús Erro <olagato@gmail.com>
2024-10-22 18:22:22 +0200

    chore: commit save point

--- termux/install.sh
+++ termux/install.sh
@@ -1,48 +1,60 @@
 #!/data/data/com.termux/files/usr/bin/zsh
 
+#!/usr/bin/env zsh
+
 # Set the installation directory
-INSTALL_DIR="/data/data/com.termux/files/home"
+INSTALL_DIR="$HOME"
 
 # Update Termux packages
-pkg update && pkg upgrade -y
+pkg update -y && pkg upgrade -y
 
-# Install essential packages including unzip and curl
+# Install essential packages
 pkg install -y git openssh lsd nano zsh wget curl
 
-# Set Zsh as the default shell
-chsh -s zsh
+# Install Oh My Zsh manually
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Clone the Oh My Zsh repository
+git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
+
+# Copy the template .zshrc to your home directory
+cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
+
+# Set Zsh as the default shell in Termux
+# Since chsh doesn't work in Termux, we can start zsh from bash
+echo "exec zsh" >> "$HOME/.bashrc"
+
+# Define ZSH_CUSTOM
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 
 # Install Oh My Zsh plugins
 
 # Zsh Autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 
 # Zsh Autocomplete
-git clone https://github.com/marlonrichert/zsh-autocomplete ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+git clone https://github.com/marlonrichert/zsh-autocomplete "$ZSH_CUSTOM/plugins/zsh-autocomplete"
 
 # Zsh Syntax Highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 
 # Install Powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
 
 # Update .zshrc to set Powerlevel10k as the default theme
-sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$INSTALL_DIR/.zshrc"
+sed -i 's|^ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' "$HOME/.zshrc"
 
 # Enable plugins in .zshrc
-sed -i '/^plugins=/c\plugins=(git zsh-autosuggestions zsh-autocomplete zsh-syntax-highlighting)' "$INSTALL_DIR/.zshrc"
+sed -i 's|^plugins=(.*)|plugins=(git zsh-autosuggestions zsh-autocomplete zsh-syntax-highlighting)|' "$HOME/.zshrc"
 
 # Reload Zsh configuration
-if [ -f "$INSTALL_DIR/.zshrc" ]; then
-    . "$INSTALL_DIR/.zshrc"
-    echo "Zsh configuration reloaded with Oh My Zsh, Powerlevel10k, and plugins."
-    exec zsh  # Restart Zsh to apply changes
+if [ -f "$HOME/.zshrc" ]; then
+    echo "Zsh configuration updated."
 else
     echo "No .zshrc file found. You may want to create one."
 fi
 
 # Notify the user
-echo "Installation complete! Zsh is now the default shell with Oh My Zsh, Powerlevel10k theme, Autosuggestions, Autocomplete, and Syntax Highlighting plugins."
+echo "Installation complete! Zsh is now installed with Oh My Zsh, Powerlevel10k theme, and the specified plugins."
+
+# Start Zsh
+exec zsh
