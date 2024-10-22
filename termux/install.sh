f8f598236b78f3a11e5fc46fc7f9f64126424e52
Jesús Erro <olagato@gmail.com>
2024-10-22 17:52:11 +0200

    chore: commit save point

--- termux/install.sh
+++ termux/install.sh
@@ -9,9 +9,6 @@ pkg update && pkg upgrade -y
 # Install essential packages including unzip and curl
 pkg install -y git openssh lsd nano zsh wget curl
 
-# Set Zsh as the default shell
-chsh -s zsh
-
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -40,13 +37,13 @@ sed -i 's|^plugins=.*|plugins=(git zsh-autosuggestions zsh-autocomplete zsh-synt
 
 # Reload Zsh configuration
 if [ -f "$INSTALL_DIR/.zshrc" ]; then
-    # Source the .zshrc file
-    source "$INSTALL_DIR/.zshrc"
-    echo "Zsh configuration reloaded with Oh My Zsh, Powerlevel10k, and plugins."
-    exec zsh  # Restart Zsh to apply changes
+    echo "Zsh configuration updated."
 else
     echo "No .zshrc file found. You may want to create one."
 fi
 
 # Notify the user
-echo "Installation complete! Zsh is now the default shell with Oh My Zsh, Powerlevel10k theme, Autosuggestions, Autocomplete, and Syntax Highlighting plugins."
+echo "Installation complete! Zsh is now installed with Oh My Zsh, Powerlevel10k theme, and the specified plugins."
+
+# Start Zsh
+exec zsh
