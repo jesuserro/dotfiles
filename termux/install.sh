f006f8f866946f0cb0b740b3138b32166faa5a44
Jesús Erro <olagato@gmail.com>
2024-10-25 18:27:26 +0200

    chore: commit save point

--- termux/install.sh
+++ termux/install.sh
@@ -15,70 +15,33 @@ chsh -s zsh
 export SHELL=$(which zsh)
 exec zsh
 
-# Instalar Oh My Zsh sin interacción
-export RUNZSH=no
-export CHSH=no
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Instalar Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# Instalar plugins de Oh My Zsh
-ZSH_CUSTOM="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"
-
-# Clonar los repositorios de los plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
-git clone https://github.com/marlonrichert/zsh-autocomplete ${ZSH_CUSTOM}/plugins/zsh-autocomplete
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM}/plugins/zsh-history-substring-search
-git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins ${ZSH_CUSTOM}/plugins/autoupdate
-git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM}/plugins/z
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
-
 # Añadir alias al .zshrc
 cat << 'EOF' >> ~/.zshrc
-
-# Alias personalizados
 alias ll="lsd -la"
 alias ups="pkg update -y && pkg upgrade -y && omz update && upgrade_oh_my_zsh_custom"
 alias git-update="git fetch --all --prune && git pull"
 alias git-save="git add -A && git commit -m 'chore: commit save point' && git push origin HEAD"
 EOF
 
-# Configurar el tema y los plugins en .zshrc
-sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
-
-# Añadir plugins al .zshrc con "zsh-syntax-highlighting" al final
-sed -i '/^plugins=/d' ~/.zshrc
-cat << 'EOF' >> ~/.zshrc
-
-# Plugins de Oh My Zsh
-plugins=(
-  autoupdate
-  aws
-  colored-man-pages
-  colorize
-  composer
-  dirhistory
-  docker
-  extract
-  gh
-  git
-  history
-  jsontools
-  vi-mode
-  wp-cli
-  zsh-autosuggestions
-  zsh-autocomplete
-  zsh-completions
-  zsh-history-substring-search
-  z
-  zsh-syntax-highlighting
-)
-EOF
-
 # Recargar .zshrc
 source ~/.zshrc
+
+# Instalar Oh My Zsh sin interacción
+export RUNZSH=no
+export CHSH=no
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Instalar plugins de Oh My Zsh
+ZSH_CUSTOM="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM}/plugins/zsh-history-substring-search
+git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins ${ZSH_CUSTOM}/plugins/autoupdate
+git clone https://github.com/marlonrichert/zsh-autocomplete ${ZSH_CUSTOM}/plugins/zsh-autocomplete
+
+# Añadir plugins al .zshrc
+sed -i 's/plugins=(git)/plugins=(autoupdate aws colored-man-pages colorize composer dirhistory docker extract gh git history jsontools vi-mode wp-cli zsh-autosuggestions zsh-autocomplete zsh-completions zsh-history-substring-search z zsh-syntax-highlighting)/g' ~/.zshrc
 
 # Configurar Git
 git config --global --add safe.directory /storage/emulated/0/Documents/vault
